1412830a0a7b7e09f28476c91bb01d7e1175ecf5
Ashutosh Deshmukh <ashutoshdeshmukhh@gmail.com>
2022-11-08 18:08:51 +0530

    q

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,51 +1,77 @@
-import { StyleSheet, Text, View, SafeAreaView, Image,ScrollView } from "react-native";
-import React ,{useContext} from "react";
-import FitnessCards from "../components/FitnessCards";
-import { FitnessItems } from "../Context";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  ScrollView,
+} from 'react-native';
+import React, { useContext } from 'react';
+import FitnessCards from '../components/FitnessCards';
+import { FitnessItems } from '../Context';
+
+import Headers from './src/components/Headers';
+import Challenges from './src/components/Challenges';
+import Workouts from './src/components/Workouts';
+
+const data = [
+  {
+    id: 1,
+    name: 'Full Body Challenge',
+    days: 30,
+    workouts: 35,
+    level: 'EASY',
+    url: 'https://cdn.pixabay.com/photo/2016/11/22/22/24/adult-1850925_960_720.jpg',
+  },
+  {
+    id: 2,
+    name: 'Begineer Abs Challenge',
+    days: 30,
+    workouts: 35,
+    level: 'EASY',
+    url: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/best-ways-to-get-abs-1296x728-feature.jpg',
+  },
+];
 
 const HomeScreen = () => {
   const {
-   
     minutes,
-  
+
     calories,
 
     workout,
   } = useContext(FitnessItems);
   return (
-    <ScrollView style={{marginTop:40}}>
+    <ScrollView style={{ marginTop: 40 }}>
       <View
         style={{
-          backgroundColor: "#CD853F",
+          backgroundColor: '#CD853F',
           padding: 10,
-          height: 200,
-          width: "100%",
-        }}
-      >
-        <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+          height: '100%',
+          width: '100%',
+        }}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>
           HOME WORKOUT
         </Text>
 
         <View
           style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
             marginTop: 20,
-          }}
-        >
+          }}>
           <View>
             <Text
               style={{
-                textAlign: "center",
-                fontWeight: "bold",
-                color: "white",
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'white',
                 fontSize: 18,
-              }}
-            >
+              }}>
               {workout}
             </Text>
-            <Text style={{ color: "#D0D0D0", fontSize: 17, marginTop: 6 }}>
+            <Text style={{ color: '#D0D0D0', fontSize: 17, marginTop: 6 }}>
               WORKOUTS
             </Text>
           </View>
@@ -53,15 +79,14 @@ const HomeScreen = () => {
           <View>
             <Text
               style={{
-                textAlign: "center",
-                fontWeight: "bold",
-                color: "white",
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'white',
                 fontSize: 18,
-              }}
-            >
+              }}>
               {calories}
             </Text>
-            <Text style={{ color: "#D0D0D0", fontSize: 17, marginTop: 6 }}>
+            <Text style={{ color: '#D0D0D0', fontSize: 17, marginTop: 6 }}>
               KCAL
             </Text>
           </View>
@@ -69,34 +94,39 @@ const HomeScreen = () => {
           <View>
             <Text
               style={{
-                textAlign: "center",
-                fontWeight: "bold",
-                color: "white",
+                textAlign: 'center',
+                fontWeight: 'bold',
+                color: 'white',
                 fontSize: 18,
-              }}
-            >
+              }}>
               {minutes}
             </Text>
-            <Text style={{ color: "#D0D0D0", fontSize: 17, marginTop: 6 }}>
+            <Text style={{ color: '#D0D0D0', fontSize: 17, marginTop: 6 }}>
               MINS
             </Text>
           </View>
         </View>
 
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
           <Image
             style={{
-              width: "90%",
+              width: '90%',
               height: 120,
               marginTop: 20,
               borderRadius: 7,
             }}
             source={{
-              uri: "https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,ar_1.2,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/sku-card-widget/gold2.png",
+              uri: 'https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_842,ar_1.2,q_auto:eco,dpr_2,f_auto,fl_progressive/image/test/sku-card-widget/gold2.png',
             }}
           />
         </View>
-        <FitnessCards/>
+        <FitnessCards />
+      </View>
+      // new
+      <View style={styles.screen}>
+        <Headers />
+        <Challenges data={data} />
+        <Workouts />
       </View>
     </ScrollView>
   );
